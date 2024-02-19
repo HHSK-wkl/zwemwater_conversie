b047850565f10bb18e93c9d310763233a1ab5006
Johan van Tent <tentvanjohan@hotmail.com>
2024-02-19 09:13:02 +0100

    code voor runnen buiten Rstudio

---
+++ chemie_diagnostics_shiny.bat
@@ -0,0 +1,1 @@
+"C:/Program Files/R/R-4.3.0/bin/x64/Rscript.exe" -e "source('C:/R/zwemwater_conversie/run.R')"
---
+++ run.R
@@ -0,0 +1,3 @@
+library(shiny)
+setwd("C:/R/zwemwater_conversie")
+runApp(launch.browser = TRUE)
